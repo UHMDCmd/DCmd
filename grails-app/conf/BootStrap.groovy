01098f0f9a7cae8c219e54594ce98ff7cfb5ec2d
Tyler Nakamura <tyler@tylernakamura.com>
2016-02-29 14:09:02 -1000

    add Adam credentials to user auth in BootStrap.groovy

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -361,8 +361,10 @@ class BootStrap {
                  def defaultSettings = new Uisettings(themeVal:1,header:1,background:1,font:1).save(failOnError: true,flush:true)
 
                 def userKarsin, userElfalan, userHodges, kawachi, ckawano, jonathan, czane, steven, garry, thang, romeot,
-                    anicasm, nakadoma, vyoshida, osamum, kc96813, userKylanh, userTylercn, userAaronvil
+                    anicasm, nakadoma, vyoshida, osamum, kc96813, userKylanh, userTylercn, userAaronvil, userAlaimana
                 if(!User.count()) {
+                    userAlaimana = new User(username: 'alaimana', password:'none', enabled: true)
+                    userAlaimana.save(failOnError:true,flush:true)
                     userAaronvil = new User(username: 'aaronvil', password:'none', enabled: true)
                     userAaronvil.save(failOnError:true,flush:true)
                     userTylercn = new User(username: 'tylercn', password:'none', enabled: true)
@@ -405,8 +407,10 @@ class BootStrap {
                 }
                 def rolesKarsin, rolesElfalan, rolesHodges, rolesKawachi, rolesCkawano, rolesJonathan, rolesCzane,
                     rolesSteven, rolesGarry, rolesThang, rolesRomeot, rolesAnicasm, rolesNakadoma, rolesVyoshida,
-                    rolesOsamum, rolesKc96813, rolesKylanh, rolesTylercn, rolesAaronvil
+                    rolesOsamum, rolesKc96813, rolesKylanh, rolesTylercn, rolesAaronvil, rolesAlaimana
                 if(!UserRole.count()) {
+                    rolesAlaimana = new UserRole(user: userAlaimana, role: ROLE_USER)
+                    rolesAlaimana.save(failOnError:true,flush:true)
                     rolesAaronvil = new UserRole(user: userAaronvil, role: ROLE_USER)
                     rolesAaronvil.save(failOnError:true,flush:true)
                     rolesTylercn = new UserRole(user: userTylercn, role: ROLE_USER)
@@ -1116,8 +1120,10 @@ class BootStrap {
 
 
                 def userKarsin, userElfalan, userHodges, kawachi, ckawano, jonathan, czane, steven, garry, thang, romeot,
-                    anicasm, nakadoma, vyoshida, osamum, kc96813, userKylanh, userJshima, userTylercn, userAaronvil
+                    anicasm, nakadoma, vyoshida, osamum, kc96813, userKylanh, userJshima, userTylercn, userAaronvil, userAlaimana
                 if(!User.count()) {
+                    userAlaimana = new User(username: 'alaimana', password:'none', enabled: true, userSettings: defaultSettings)
+                    userAlaimana.save(failOnError:true,flush:true)
                     userAaronvil = new User(username: 'aaronvil', password:'none', enabled: true, userSettings: defaultSettings)
                     userAaronvil.save(failOnError:true,flush:true)
                     userTylercn = new User(username: 'tylercn', password:'none', enabled: true, userSettings: defaultSettings)
@@ -1162,8 +1168,10 @@ class BootStrap {
                 }
                 def rolesKarsin, rolesElfalan, rolesHodges, rolesKawachi, rolesCkawano, rolesJonathan, rolesCzane,
                         rolesSteven, rolesGarry, rolesThang, rolesRomeot, rolesAnicasm, rolesNakadoma, rolesVyoshida,
-                        rolesOsamum, rolesKc96813, rolesKylanh, rolesJshima, rolesTylercn, rolesAaronvil
+                        rolesOsamum, rolesKc96813, rolesKylanh, rolesJshima, rolesTylercn, rolesAaronvil, rolesAlaimana
                 if(!UserRole.count()) {
+                    rolesAlaimana = new UserRole(user: userAlaimana, role: ROLE_ADMIN)
+                    rolesAlaimana.save(failOnError:true,flush:true)
                     rolesAaronvil = new UserRole(user: userAaronvil, role: ROLE_ADMIN)
                     rolesAaronvil.save(failOnError:true,flush:true)
                     rolesTylercn = new UserRole(user: userTylercn, role: ROLE_ADMIN)
